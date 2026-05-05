6b956ecde003db8bc794eba453f1741272d86ee6
sethcarl2000 <sethhall@jlab.org>
2026-05-05 00:41:22 -0400

    Add namespace of const numbers

---
+++ numbers.hxx
@@ -0,0 +1,13 @@
+#ifndef numbers_HXX
+#define numbers_HXX
+
+#include <limits> 
+
+namespace numbers {
+    
+    constexpr double pi = 3.14159265359; 
+
+    constexpr double NaN = std::numeric_limits<double>::quiet_NaN(); 
+}
+
+#endif
