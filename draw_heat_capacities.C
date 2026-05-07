314f3862d58d8c0058392312e6ec9935c9827ee3
sethcarl2000 <sethhall@jlab.org>
2026-05-07 15:48:19 -0400

    Add drawing of Tc at different scaling sizes

--- draw_heat_capacities.C
+++ draw_heat_capacities.C
@@ -9,6 +9,9 @@
 #include <TH2D.h> 
 #include <TAxis.h> 
 #include <TLegend.h> 
+#include <TF1.h> 
+#include <TFitResult.h>
+#include <TFitResultPtr.h> 
 
 //stdlib
 #include <string>
@@ -29,7 +32,75 @@ namespace {
         for (double x : v) { max = std::max(x,max); }
         return max; 
     };
+
+    void fit_gaus_to_vectors( 
+        const std::vector<double>& pts_x,
+        const std::vector<double>& pts_y,   
+        double radius, 
+        double &center, 
+        double &sigma, 
+        bool do_draw=false
+    ) 
+    {
+        //first guess for the center of the fit. we assume here that the gaus we wanna fit is the tallest peak in the hist. 
+        auto g = new TGraph(pts_x.size(), pts_x.data(), pts_y.data()); 
+
+        double y_max=pts_y.front();
+        double y_max_x=pts_x.front(); 
+        for (int i=0; i<pts_x.size(); i++) {
+            if (y_max < pts_y[i]) {
+                y_max_x = pts_x[i];
+                y_max = pts_y[i]; 
+            }
+        }
+
+        auto x_ax = g->GetXaxis(); 
+        center = y_max_x; 
+        
+        //first guess for sigma 
+        sigma = radius/8.; 
+
+        double x_min = std::max( center-radius, x_ax->GetXmin() );    
+        double x_max = std::min( center+radius, x_ax->GetXmax() );
+
+        //the guess for the 'base' will be based on the bins on either extreme end of the fit
+        double base = 0.5*(
+            g->Eval(center + radius) + g->Eval(center - radius)
+        ); 
+
+        //amplitude over background
+        double amplitude = y_max - base; 
+        
+        //our function to fit the histogram with   
+        auto gaus_fit = new TF1( "gaus_fit", [](const double* X, const double *par){
+            double arg = (X[0]-par[1])/par[2]; 
+            return par[0]*std::exp( -arg*arg/2. ) + par[3]; 
+        }, x_min, x_max,  4); 
+    
+        //set the parameters 
+        gaus_fit->SetParameter( 0, amplitude ); 
+        gaus_fit->SetParameter( 1, center ); 
+        gaus_fit->SetParameter( 2, sigma );  
+        gaus_fit->SetParameter( 3, base ); 
+
+        std::printf("amplitude: %f center: %f sigma %f base %f\n", amplitude,center,sigma,base); 
+        
+        auto fit_ptr = g->Fit("gaus_fit", (do_draw ? "S L R Q" : "S L N R Q")); 
+        
+        if (!fit_ptr.Get() || fit_ptr->IsValid()==false) {
+            //throw std::logic_error("<fit_gaus_to_hist>: fit failed."); 
+            Error(__func__, "Fit failed"); 
+            return; 
+        }
+
+        center = fit_ptr->Parameter(1); 
+        sigma  = std::fabs(fit_ptr->Parameter(2)); 
+        
+        return; 
+    }
 }
+
+
 
 int draw_heat_capacities()
 {
@@ -50,6 +121,9 @@ int draw_heat_capacities()
 
     canvC->Divide(1, n_max-n_min+1, 0.,0.01);
     canvE->Divide(1, n_max-n_min+1, 0.,0.01); 
+
+    //check the finite temperature scaling 
+    std::vector<double> pts_N, pts_tc_cool, pts_tc_hot; 
 
     for (int n=n_min; n<=n_max; n++) {
 
@@ -78,19 +152,28 @@ int draw_heat_capacities()
             canvC->cd(n-n_min+1); 
             graph_frameC->Draw(); 
 
+            double center, sigma; 
+            
             gcC = new TGraph(pts_cg2.size(), pts_cg2.data(), pts_cC.data()); 
             //gcC->SetLineColor(n-n_min+1); 
             gcC->SetLineStyle(kDashed); 
             gcC->SetLineWidth(3); 
             gcC->SetLineColor(kBlue);
             gcC->Draw("SAME"); 
-            
+            fit_gaus_to_vectors(pts_cg2, pts_cC, 0.025, center, sigma, true); 
+            pts_tc_cool.push_back(center); 
+
             ghC = new TGraph(pts_hg2.size(), pts_hg2.data(), pts_hC.data()); 
             //ghC->SetLineColor(n-n_min+1); 
             ghC->SetLineStyle(kSolid); 
             ghC->SetLineWidth(3); 
             ghC->SetLineColor(kRed);
             ghC->Draw("SAME"); 
+            fit_gaus_to_vectors(pts_hg2, pts_hC, 0.025, center, sigma, true); 
+            pts_tc_hot.push_back(center); 
+
+            pts_N.push_back( 4.*std::pow(n,4) );
+            
             
             if (n==n_min) {
                 auto legend = new TLegend; 
@@ -138,6 +221,51 @@ int draw_heat_capacities()
 
     //legend->Draw(); 
     canvC->SaveAs("Heat_capacity.png"); 
+
+    new TCanvas;
+    auto legend = new TLegend; 
+    
+    auto g_cool = new TGraph(pts_N.size(), pts_N.data(), pts_tc_cool.data());
+    g_cool->SetMaximum(0.5);
+    g_cool->SetMinimum(0.1);
+    g_cool->SetTitle("Phase transition temperature;N = 4*L^{4};T_{c}"); 
+    g_cool->SetLineStyle(kDashed); 
+    g_cool->SetLineWidth(3); 
+    g_cool->SetLineColor(kBlue);
+    g_cool->SetMarkerStyle(kOpenCircle);
+    g_cool->SetMarkerColor(kBlue);  
+    g_cool->SetMarkerSize(1);
+    g_cool->Draw(); 
+
+    auto scaling_fit = new TF1("fit_temp", [](const double *x, const double* par){
+        return par[0]*std::pow( x[0]/par[1], -4. ) + par[2];
+    }, pts_N.front(), pts_N.back(), 3);
+
+    scaling_fit->SetParameter( 0, 0. );
+    scaling_fit->SetParameter( 1, 1.e3 );
+    scaling_fit->SetParameter( 2, pts_tc_cool.back() );
+
+    g_cool->Fit("fit_temp", "R N Q"); 
+    scaling_fit->SetLineColor(kBlue); 
+    //scaling_fit->DrawCopy("SAME"); 
+
+    auto g_hot = new TGraph(pts_N.size(), pts_N.data(), pts_tc_hot.data());
+    g_hot->SetMaximum(0.5);
+    g_hot->SetMinimum(0.1);
+    g_hot->SetTitle("Phase transition temperature;N = 4*L^{4};T_{c}"); 
+    g_hot->SetLineStyle(kSolid); 
+    g_hot->SetLineWidth(3); 
+    g_hot->SetLineColor(kRed);
+    g_hot->SetMarkerStyle(kOpenCircle);
+    g_hot->SetMarkerColor(kRed);  
+    g_hot->SetMarkerSize(1);
+    g_hot->Draw("SAME, PL"); 
+
+    scaling_fit->SetParameter( 2, pts_tc_hot.back() );
+
+    g_hot->Fit("fit_temp", "R N Q"); 
+    scaling_fit->SetLineColor(kRed); 
+    //scaling_fit->DrawCopy("SAME"); 
 
     return 0; 
 }
